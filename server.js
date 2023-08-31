d3354db8741004f4fb4feaa264b5a4e94d4b4adf
Shashwat <shashlko2002@gmai.com>
2023-08-31 15:06:54 +0530

    first commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/test", testRoute);
 /*Mongoose Setup*/
 const PORT = process.env.PORT;
 const connectionSting =
-  process.env.MONGO_URL || "mongodb+srv://shashlko2002:RypHZHOpnTDir1lG@cluster0.bgyqhxd.mongodb.net/";
+  process.env.MONGO_URL;
 
 mongoose
   .connect(connectionSting, {
