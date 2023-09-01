3279263b5b277079de212605453898a59d8a1d10
Shashwat <shashlko2002@gmai.com>
2023-09-01 14:38:06 +0530

    used double qoutes in sameSite

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
     sameSite: "none",
-    secure: true,
+    secure: "true",
   },
   store: MongoStore.create({ mongoUrl:process.env.MONGO_URL})
 }));
