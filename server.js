198dad97d311c327c9f4323c2f38400f1e5eab07
Shashwat <shashlko2002@gmai.com>
2023-09-02 00:09:00 +0530

    added secure

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24,
     sameSite: "none",
+    secure: true
   },
   store: MongoStore.create({ mongoUrl:process.env.MONGO_URL})
 }));
